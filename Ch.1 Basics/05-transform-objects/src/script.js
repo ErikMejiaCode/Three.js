a88d203a6109445cb73993b68f11d2130320fabb
Erik Mejia <erikdojo352@gmail.com>
2023-02-26 20:58:21 -0800

    Position, Scale, Rotation & Groups

--- Ch.1 Basics/05-transform-objects/src/script.js
+++ Ch.1 Basics/05-transform-objects/src/script.js
@@ -9,17 +9,66 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-mesh.position.x = 0.7
-mesh.position.y = -0.6
-mesh.position.z = 1
-scene.add(mesh)
+
+const group = new THREE.Group()
+group.position.y = 0.5
+group.scale.y = 1.5
+group.rotation.y = 1
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color : 0xff0000})
+)
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color : 0x00ff00})
+)
+cube2.position.x = -1.5
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color : 0x0000ff})
+)
+cube3.position.x = 1.5
+group.add(cube3)
+
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const mesh = new THREE.Mesh(geometry, material)
+// scene.add(mesh)
+
+//Position
+// mesh.position.x = 0.7
+// mesh.position.y = -0.6
+// mesh.position.z = 1
+//positions can be updated at once, as opposed to seperately (above)
+// mesh.position.set(0.7, -0.6, 1)
+
+//Scale
+// mesh.scale.x = 2
+// mesh.scale.y = 0.5
+// mesh.scale.z = 0.5
+// mesh.scale.set(2, 0.5, 0.5)
+
+// Rotation
+// mesh.rotation.reorder('YXZ')
+// mesh.rotation.x = Math.PI * 0.25
+// mesh.rotation.y = Math.PI * 0.25
+
+//AXES Helper
+const axesHelper = new THREE.AxesHelper(2)
+scene.add(axesHelper)
+
+// mesh.position.normalize()
+// console.log(mesh.position.length())
 
 /**
  * Sizes
- */
+*/
 const sizes = {
     width: 800,
     height: 600
@@ -27,10 +76,14 @@ const sizes = {
 
 /**
  * Camera
- */
+*/
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
+
+// camera.lookAt(mesh.position)
+
+// console.log(mesh.position.distanceTo(camera.position))
 
 /**
  * Renderer
