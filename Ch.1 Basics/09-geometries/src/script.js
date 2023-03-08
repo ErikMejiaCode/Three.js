bd3e1e01d956884b22f9ac68390ae51cdef073e9
Erik Mejia <erikdojo352@gmail.com>
2023-03-07 21:14:13 -0500

    Creating empty geometries with BufferGeometry

--- Ch.1 Basics/09-geometries/src/script.js
+++ Ch.1 Basics/09-geometries/src/script.js
@@ -11,8 +11,57 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
+
+// Creating our own empty geometry
+const geometry = new THREE.BufferGeometry()
+
+// How many triagles we want 
+// Float Arrays is an optimized array in which we can only store floats 
+const count = 500 
+const positionsArray = new Float32Array(count * 3 * 3)
+
+for(let i = 0; i < count * 3 * 3; i++){
+    positionsArray[i] = (Math.random() - 0.5) * 4
+}
+
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
+geometry.setAttribute('position', positionsAttribute)
+
+// const positionsArray = new Float32Array(
+//     // [
+//     // 0, 0, 0, // First Vertice
+//     // 0, 1, 0, // Second Vertice
+//     // 1, 0, 0, // Third Vertice 
+//     // ]
+// )
+
+// const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
+
+// geometry.setAttribute('position', positionsAttribute)
+
+
+//One way of doing it 
+// // First Vertice
+// positionsArray[0] = 0
+// positionsArray[1] = 0
+// positionsArray[2] = 0
+
+// // Second Vertice
+// positionsArray[3] = 0
+// positionsArray[4] = 1
+// positionsArray[5] = 0
+
+// // Third Vertice 
+// positionsArray[6] = 1
+// positionsArray[7] = 0
+// positionsArray[8] = 0
+
+
+const material = new THREE.MeshBasicMaterial({ 
+    color: 0xff0000,
+    wireframe: true
+})
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
