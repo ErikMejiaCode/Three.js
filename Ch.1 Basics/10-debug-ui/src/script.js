12d1a2e9ea070041aafc4dc79ee438dbff8be081
Erik Mejia <erikdojo352@gmail.com>
2023-03-13 13:31:26 -0400

    Debug panel added using dat.GUI

--- Ch.1 Basics/10-debug-ui/src/script.js
+++ Ch.1 Basics/10-debug-ui/src/script.js
@@ -1,6 +1,31 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import gsap from 'gsap'
+import * as dat from 'dat.gui'
+
+/**
+ * Debug
+ */
+const gui = new dat.GUI()
+// //Function for spin will also be added to the parameters below
+// function spin(){
+// }
+
+// Used to get the color for Debug
+const parameters = {
+    color: 0xff0000,
+    spin: () => {
+        gsap.to(mesh.rotation, { duration: 1, y: mesh.rotation.y + Math.PI * 2 })
+    }
+}
+gui 
+    .addColor(parameters, 'color')
+    .onChange(() => {
+        material.color.set(parameters.color)
+    })
+
+gui
+    .add(parameters, 'spin')
 
 /**
  * Base
@@ -15,9 +40,26 @@ const scene = new THREE.Scene()
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshBasicMaterial({ color: parameters.color })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+
+// Adding elements to the debug UI
+// Debug 
+// gui.add(mesh.position, 'y', -3, 3, 0.01)
+gui
+    .add(mesh.position, 'y')
+    .min(-3)
+    .max(3)
+    .step(0.01)
+    .name('elevation')
+
+gui
+    .add(mesh, 'visible')
+
+gui
+    .add(material, 'wireframe')
+
 
 /**
  * Sizes
