ed642292ea503aa7825633b877cf2ca35f611d8a
Erik Mejia <erikdojo352@gmail.com>
2023-03-15 10:56:28 -0400

    Using Meshes/Lights/Environment Map

--- Ch.1 Basics/12-materials/src/script.js
+++ Ch.1 Basics/12-materials/src/script.js
@@ -1,12 +1,40 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
+
+/**
+ * Creating Debug UI from scratch 
+ */
+const gui = new dat.GUI()
+
 
 /**
  * Textures 
  */
 const textureLoader = new THREE.TextureLoader()
+const cubeTextureLoader = new THREE.CubeTextureLoader()
 
-const doorColorTexture = textureLoader.load('/textures/door')
+const doorColorTexture = textureLoader.load('/textures/door/color.jpg')
+const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+const doorAmbientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+const doorHeightTexture = textureLoader.load('/textures/door/height.jpg')
+const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
+const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+const matcapTexture = textureLoader.load('/textures/matcaps/8.png')
+const gradientTexture = textureLoader.load('/textures/gradients/5.jpg')
+gradientTexture.minFilter = THREE.NearestFilter
+gradientTexture.magFilter = THREE.NearestFilter
+gradientTexture.generateMipmaps = false
+
+const environemntMapTexture = cubeTextureLoader.load([
+    '/textures/environmentMaps/4/px.png',
+    '/textures/environmentMaps/4/nx.png',
+    '/textures/environmentMaps/4/py.png',
+    '/textures/environmentMaps/4/ny.png',
+    '/textures/environmentMaps/4/pz.png',
+    '/textures/environmentMaps/4/nz.png',
+])
 
 /**
  * Base
@@ -20,26 +48,115 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const material = new THREE.MeshBasicMaterial( {color: 0xff0000} )
+// //Mesh Basic Material 
+// const material = new THREE.MeshBasicMaterial()
+// material.map = doorColorTexture
+// material.color.set('#ff00ff')
+// material.color = new THREE.Color('pink')
+// material.wireframe = true
+// material.opacity = 0.5
+// material.transparent = true
+// material.alphaMap = doorAlphaTexture
+// material.side = THREE.DoubleSide
+
+// // Mesh Normal Material 
+// const material = new THREE.MeshNormalMaterial()
+// material.wireframe = true
+// material.flatShading = true
+
+// // Mesh MatCap Material 
+// const material = new THREE.MeshMatcapMaterial()
+// material.matcap = matcapTextures
+
+// // Mesh Depth Material 
+// const material = new THREE.MeshDepthMaterial()
+
+// //Mesh Lambert Material
+// const material = new THREE.MeshLambertMaterial()
+
+// //Mesh Phong Material
+// const material = new THREE.MeshPhongMaterial()
+// material.shininess = 1000
+// material.specular = new THREE.Color(0x1188ff)
+
+// //Mesh Toon Material 
+// const material = new THREE.MeshToonMaterial()
+// material.gradientMap = gradientTexture
+
+// // Mesh Standard Material
+// const material = new THREE.MeshStandardMaterial()
+// material.metalness = 0
+// material.roughness = 1
+// material.map = doorColorTexture
+// material.aoMap = doorAmbientOcclusionTexture
+// material.aoMapIntensity = 1
+// material.displacementMap = doorHeightTexture
+// material.displacementScale = 0.05
+// material.metalnessMap = doorMetalnessTexture
+// material.roughnessMap = doorRoughnessTexture
+// material.normalMap = doorNormalTexture
+// material.normalScale.set(0.5, 0.5)
+// material.transparent = true
+// material.alphaMap = doorAlphaTexture
+
+// Environment Map 
+const material = new THREE.MeshStandardMaterial()
+material.metalness = 0.7
+material.roughness = 0.2
+material.envMap = environemntMapTexture
+
+
+//Adding Debug for material & geometry as you create it 
+gui.add(material, 'metalness').min(0).max(1).step(0.0001)
+gui.add(material, 'roughness').min(0).max(1).step(0.0001)
+gui.add(material, 'aoMapIntensity').min(1).max(10).step(0.0001)
+gui.add(material, 'displacementScale').min(0).max(1).step(0.0001)
+
+
 //Creating meshes
 const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.SphereGeometry(0.5, 64, 64),
     material
 )
 sphere.position.x = -1.5
+// Updating Uv values and saving them into UV2
+sphere.geometry.setAttribute(
+    'uv2', 
+    new THREE.BufferAttribute(sphere.geometry.attributes.uv.array, 2))
 
 const plane = new THREE.Mesh(
-    new THREE.PlaneGeometry(1, 1),
+    new THREE.PlaneGeometry(1, 1, 100, 100),
     material
 )
+// Updating Uv values and saving them into UV2
+plane.geometry.setAttribute(
+    'uv2', 
+    new THREE.BufferAttribute(plane.geometry.attributes.uv.array, 2))
 
 const torus = new THREE.Mesh(
-    new THREE.TorusGeometry(0.3, 0.2, 16, 32),
+    new THREE.TorusGeometry(0.3, 0.2, 64, 128),
     material
 )
 torus.position.x = 1.5
+torus.geometry.setAttribute(
+    'uv2', 
+    new THREE.BufferAttribute(torus.geometry.attributes.uv.array, 2))
 
 scene.add(sphere, plane, torus)
+
+/**
+ * Lights 
+ */
+
+// Adding an ambient light 
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+scene.add(ambientLight)
+
+const pointLight = new THREE.PointLight(0xffffff, 0.5)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight)
 
 /**
  * Sizes
