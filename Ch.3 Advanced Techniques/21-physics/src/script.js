da6b92358fec44aabb632fcd6eff4a8327cb6142
Erik Mejia <erikdojo352@gmail.com>
2023-04-02 20:58:14 -0400

    added cannon-es

--- Ch.3 Advanced Techniques/21-physics/src/script.js
+++ Ch.3 Advanced Techniques/21-physics/src/script.js
@@ -1,7 +1,8 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "lil-gui";
-import CANNON from "cannon";
+// import CANNON from "cannon";
+import * as CANNON from "cannon-es";
 
 /**
  * Debug
