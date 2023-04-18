cd3d23b03165cca240276915fab67eb91bd0f242
Erik Mejia <erikdojo352@gmail.com>
2023-04-18 12:08:42 -0400

    Camera class and orbit controls

--- Ch.3 Advanced Techniques/26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ Ch.3 Advanced Techniques/26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -1,8 +1,18 @@
+import * as THREE from "three";
 import Sizes from "./Utils/Sizes";
 import Time from "./Utils/Time";
+import Camera from "./Camera";
+
+let instance = null;
 
 export default class Experience {
   constructor(canvas) {
+    //Setting up the Singleton
+    if (instance) {
+      return instance;
+    }
+    instance = this;
+
     //Global access
     window.experience = this;
 
@@ -12,6 +22,8 @@ export default class Experience {
     // SetUp
     this.sizes = new Sizes();
     this.time = new Time();
+    this.scene = new THREE.Scene();
+    this.camera = new Camera();
 
     //Sizes resize event
     this.sizes.on("resize", () => {
@@ -24,7 +36,11 @@ export default class Experience {
     });
   }
 
-  resize() {}
+  resize() {
+    this.camera.resize();
+  }
 
-  update() {}
+  update() {
+    this.camera.update();
+  }
 }
