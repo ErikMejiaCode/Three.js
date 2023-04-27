06b680225bb26e0aed2d0005ff431322e6a58f38
Erik Mejia <erikdojo352@gmail.com>
2023-04-26 20:33:01 -0700

    passing uniforms to animate / color flag

--- Ch.4 Shaders/27-shaders/src/shaders/test/vertex.glsl
+++ Ch.4 Shaders/27-shaders/src/shaders/test/vertex.glsl
@@ -1,13 +1,22 @@
 uniform mat4 projectionMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 modelMatrix;
+uniform vec2 uFrequency;
+uniform float uTime;
 
 attribute vec3 position;
+attribute float aRandom;
+
+// varying float vRandom;
 
 void main()
 {   
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-    modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
+    modelPosition.z += sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
+    modelPosition.z += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
+    // modelPosition.z += aRandom * 0.1;
+
+    // vRandom = aRandom;
     
 
     vec4 virePosition = viewMatrix * modelPosition;
