6133ccb07c4f8d92adde41f2e0b00ece5f3974df
Erik Mejia <erikdojo352@gmail.com>
2023-04-26 20:58:31 -0700

    color variations

--- Ch.4 Shaders/27-shaders/src/shaders/test/fragment.glsl
+++ Ch.4 Shaders/27-shaders/src/shaders/test/fragment.glsl
@@ -5,10 +5,12 @@ uniform sampler2D uTexture;
 
 // varying float vRandom;
 varying vec2 vUv;
+varying float vElevation;
 
 void main()
 {
     vec4 textureColor = texture2D(uTexture, vUv);
+    textureColor.rgb *= vElevation * 2.0 + 0.7;
     gl_FragColor =  textureColor;
     // gl_FragColor =  vec4(uColor, 1.0);
 }
